6f5af553b0bc7630eaddbbf498d4acb8d0528da4
Graeme Robinson <graeme.robinson@ymail.com>
2026-07-28 23:47:49 -0600

    Modified LLM provider to be read from an env variuable (and removed unused functions from utils.py)

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,3 @@
-from .utils import create_index, create_search_index, check_index_ready, set_env, get_llm
+from .utils import get_llm
 
-__all__ = ["create_index", "create_search_index", "check_index_ready","set_env", "get_llm"]
+__all__ = ["get_llm"]
