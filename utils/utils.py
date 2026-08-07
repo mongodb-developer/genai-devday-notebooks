d73e1a2bc15a7956e03296dcd615ee5a09996a8b
Graeme Robinson <graeme.robinson@ymail.com>
2026-08-06 19:19:38 -0600

    updated LLM models for consistency with main lab branch

--- utils/utils.py
+++ utils/utils.py
@@ -3,29 +3,35 @@ from langchain_openai import AzureChatOpenAI
 from langchain_google_genai import ChatGoogleGenerativeAI
 import os
 
-SLEEP_TIMER = 5
-PROXY_ENDPOINT = "https://vtqjvgchmwcjwsrela2oyhlegu0hwqnw.lambda-url.us-west-2.on.aws/"
+MAX_TOKENS = 4096
 
-def get_llm():
+def get_llm(provider: str):
+    """
+    Get the appropriate LLM instance based on the provider.
 
-    provider = os.environ.get("AILABS_LLM_PROVIDER", "aws")
+    Args:
+        provider (str): Name of the provider. One of "aws", "google", "microsoft"
+    """
     if provider == "aws":
         return ChatBedrock(
-            model_id="global.anthropic.claude-sonnet-4-5-20250929-v1:0",
+            model_id="global.anthropic.claude-sonnet-4-6",
             model_kwargs=dict(temperature=0),
             region_name="us-west-2",
+            max_tokens=MAX_TOKENS,
         )
     elif provider == "google":
         return ChatGoogleGenerativeAI(
             model="gemini-2.5-pro",
             temperature=0,
+            max_tokens=MAX_TOKENS,
         )
     elif provider == "microsoft":
         return AzureChatOpenAI(
             azure_endpoint="https://gai-326.openai.azure.com/",
-            azure_deployment="gpt-4.1",
+            azure_deployment="gpt-5.1",
             api_version="2024-12-01-preview",
             temperature=0,
+            max_tokens=MAX_TOKENS,
         )
     else:
         raise Exception("Unsupported provider. provider can be one of 'aws', 'google', 'microsoft'.")
