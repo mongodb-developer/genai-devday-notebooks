31616465aebcc2f5b0b2ce4fb0b3ee3a0069a989
sis0k0 <stanimira.vlaeva@mongodb.com>
2026-09-14 18:36:40 +0300

    Install required dependencies when starting a Codespace

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+echo ✅ APT GET UPDATE
+echo ✅ --------------
+apt-get update -y
+
+echo ✅ Install Jupyter notebook
+echo ✅ ------------------------
+apt-get install python3 python3-pip -y --no-install-recommends  
+apt-get install jupyter-notebook -y --no-install-recommends  
+
+echo ✅ Install Jupyter Python kernel
+echo ✅ -----------------------------
+# Python Kernel
+apt-get install python3-pymongo -y --no-install-recommends
+apt-get clean packages
+
+echo ✅ Install cURL
+echo ✅ ----------------
+apt-get install curl -y
+apt-get clean packages
+
+echo ✅ Install Node.js v23 
+echo ✅ ----------------
+# Install Node.js 23 as MongoDB MCP server needs at least v20 (https://github.com/mongodb-js/mongodb-mcp-server?tab=readme-ov-file#prerequisites)
+apt-get install -y --no-install-recommends npm
+npm install -g n
+n 23
+hash -r 
